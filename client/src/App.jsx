06c69687385d8bccd7ffd28cabd16455251ece73
Hassan Muhashamy <hassan.muhashamy@gmail.com>
2020-08-30 22:50:17 +0300

    Create client

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,8 +3,10 @@ import ReactDOM from "react-dom";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "shards-ui/dist/css/shards.min.css";
-import "./index.css";
 
-const App = () => <div>Hi there, I'm React from Webpack 5.</div>;
+import "./index.css";
+import Chat from "./Chat";
+
+const App = () => <Chat />;
 
 ReactDOM.render(<App />, document.getElementById("app"));
