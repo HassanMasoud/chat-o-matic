8d5098853e7e8f933eb271e469c81180e4289299
Hassan Muhashamy <hassan.muhashamy@gmail.com>
2020-08-30 23:45:38 +0300

    Style the messages

--- client/src/Chat.jsx
+++ client/src/Chat.jsx
@@ -7,6 +7,8 @@ import {
   gql,
 } from "@apollo/client";
 
+import { Container } from "shards-react";
+
 const client = new ApolloClient({
   uri: "http://localhost:4000",
   cache: new InMemoryCache(),
@@ -27,14 +29,38 @@ const Messages = ({ user }) => {
   if (!data) {
     return null;
   }
-  return JSON.stringify(data);
+  return (
+    <>
+      {data.messages.map(({ id, user: messageUser, content }) => (
+        <div
+          style={{
+            display: "flex",
+            justifyContent: user === messageUser ? "flex-end" : "flex-start",
+            paddingBottom: "1em",
+          }}
+        >
+          <div
+            style={{
+              backgroundColor: user === messageUser ? "#58bf56" : "#e5e6ea",
+              color: user === messageUser ? "white" : "black",
+              padding: "1em",
+              borderRadius: "1em",
+              maxWidth: "60%",
+            }}
+          >
+            {content}
+          </div>
+        </div>
+      ))}
+    </>
+  );
 };
 
 const Chat = () => {
   return (
-    <div>
+    <Container>
       <Messages user="Jack" />
-    </div>
+    </Container>
   );
 };
 
